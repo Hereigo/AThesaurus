bdc6a1c5e361d9d3c55b318440b8315b123ede63
Hereigo <Hereigo@online.ua>
2017-10-08 09:05:13 +0300

    Add todo.

--- Thesaurus/Thesaurus/Class1.cs
+++ Thesaurus/Thesaurus/Class1.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*
+Prior to C# code test review we would like to see effort beeing put into the Theasurus problem {input + expected output} highlighted by test cases. 
+Remember that the Theasaurus problem presents many edge-cases and has many interpretations. 
+Also we would like the solutions provided to reach production quality.   
 
-// TODO:
-// you shall be able to describe your code and the different
-// decisions you made about functionality in the two tasks below.Any reflections, deficiencies, or
-// limitations shall also be described. You may use any framework you want.
-// Preferably, the solutions should have a personal touch and reflect your mindset regarding good code
-// quality.
-// 1)  Focus on implementing the IThesaurus interface specified below and then use it in a simple
-// 2)  Using WPF, show a MessageBox on button click according to the picture below.Write unit tests
-// that verifies this code.
+We want the engineer to think himself and present his thoughts in a follow-up meeting, hence not everything is specified. This doesn't mean that we want a small/quick solution but you have a chance to to show all your knowledge-arsenal. 
+
+you shall be able to describe your code and the different decisions you made about functionality in the two tasks below.Any reflections, deficiencies, or limitations shall also be described. You may use any framework you want.
+Preferably, the solutions should have a personal touch and reflect your mindset regarding good code quality.
+1)  Focus on implementing the IThesaurus interface specified below and then use it in a simple
+2)  Using WPF, show a MessageBox on button click according to the picture below.Write unit tests that verifies this code.
+*/
 
 namespace Thesaurus
 {
