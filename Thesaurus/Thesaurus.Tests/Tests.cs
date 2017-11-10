09a85cba25fd5bbae84a1c46e7ae9e6efb0aea58
Andrew Plakhtiy <aplakhtiy@ibox.ua>
2017-11-10 19:24:49 +0200

    tests.

---
+++ Thesaurus/Thesaurus.Tests/Tests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Thesaurus.Tests
+{
+    public class Tests
+    {
+        Thesaurus thesaurus = new Thesaurus();
+
+        [Theory]
+        [InlineData("", "", "")]
+        [InlineData(" ", " ", " ")]
+        [InlineData("!%$@!&#^_+", "_)&)(^&*(%$", "(*^&%*&%#&$@")]
+        [InlineData("", "", "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890")]
+        public void AddSynonyms_WithoutExceptions(string s1, string s2, string s3)
+        {
+            IEnumerable<string> strings = new List<string> { s1, s2, s3 }; 
+
+            var excep = Record.Exception(() => thesaurus.AddSynonyms(strings));
+            Assert.Null(excep);
+        }
+    }
+}
